23193ef6203e720fca3fceb2e694493331c5e836
nihalmv-ops <nihalmv86@gmail.com>
2026-05-08 14:51:20 +0530

    fixed Django settings path

--- forest-web/forest/forest/wsgi.py
+++ forest-web/forest/forest/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'forest_managment.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'forest.settings')
 
 application = get_wsgi_application()
